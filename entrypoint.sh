ad76e88e915146c6c06e0d2f20b99a639132254d
trideepnag1911 <trideepnag@gmail.com>
2022-06-17 08:37:48 -0700

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-curl -X PUT -H -u $USER_NAME:$PASSWORD -T "$FILE" "http://34.122.63.81:8082/artifactory/gradle-dev-local/app1.jar"
+echo $USER_NAME $PASSWORD
+curl -X PUT -H -u $USER_NAME:$PASSWORD -T "$FILE" "$REGISTRY"
 #curl -H "X-JFrog-Art-Api:$PASSWORD" -X PUT "${REGISTRY}" -T "${FILE}"
