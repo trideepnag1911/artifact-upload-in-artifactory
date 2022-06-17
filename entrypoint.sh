bfd1bf9f6886507a062f8e714a8941836badfa81
trideepnag1911 <trideepnag@gmail.com>
2022-06-17 03:28:07 -0700

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-curl -X PUT -u "$USER_NAME":"$PASSWORD" -T "$FILE" '$REGISTRY'
+curl -X PUT -u "$USER_NAME":"$PASSWORD" -T "$FILE" '"$REGISTRY"'
