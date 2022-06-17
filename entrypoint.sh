e773808297ca608c3eb6d1b511335ed7d5a41999
trideepnag1911 <trideepnag@gmail.com>
2022-06-17 09:21:53 -0700

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-curl -X PUT "$REGISTRY" -T "$FILE"
+curl -X PUT -u "$USER_NAME":"$PASSWORD" "$REGISTRY" -T "$FILE"
 #curl -H "X-JFrog-Art-Api:$PASSWORD" -X PUT "${REGISTRY}" -T "${FILE}"
+if [ $? -eq 0 ]
+then
+echo "Completed"
+exit 0
+else
+echo "Failed"
+exit 1
+fi
